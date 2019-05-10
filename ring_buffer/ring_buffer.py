a9686ef34ff9bdd475b22ca0a073a50ff25c070c
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-05-10 10:09:22 -0500

    Wrote the appened method for the ring-buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,24 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    if self.current < self.capacity:
+      self.storage[self.current] = item
+      self.current += 1
+    else:
+      self.current = 0
+      self.storage[self.current] = item
+      self.current += 1
 
   def get(self):
     pass
+
+rb = RingBuffer(5)
+
+rb.append(5)
+rb.append(6)
+rb.append(3)
+rb.append(2)
+rb.append(7)
+rb.append(10)
+
+print(rb.storage)
